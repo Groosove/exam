d2c5279ee9b8473a64677279c4f8d719a8bc36f4
Fenix Lavon <flavon@si-h2.kzn.21-school.ru>
2020-11-19 14:15:11 +0300

    exam rank 04

---
+++ exam_rank_04/microshell.c
@@ -0,0 +1,150 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct s_fd
+{
+	int write;
+	int read;
+	int flag;
+}				t_fd;
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int ft_d_strlen(char **av)
+{
+	int i = 0;
+	while (av[i])
+		i++;
+	return (i);
+}
+
+void	ft_error(char *str)
+{
+	write(2, str, ft_strlen(str));
+}
+
+void	exit_fatal(void)
+{
+	ft_error("error: fatal\n");
+	exit(EXIT_FAILURE);
+}
+
+void	get_fd(t_fd *fd)
+{
+	int fds[2];
+
+	if (pipe(fds) == -1) 
+		exit_fatal();
+	fd->write = fds[1];
+	fd->read = fds[0];
+	fd->flag = 1;
+}
+
+void	cd(char **av)
+{
+	if (ft_d_strlen(av) != 2)
+		ft_error("error: cd: bad arguments\n");
+	else if (chdir(av[1]))
+	{
+		ft_error("error: cd: cannot change directory to ");
+		ft_error(av[1]);
+		ft_error("\n");
+	}
+}
+
+void	start_process(char **av, char **envp)
+{
+	pid_t pid;
+	if ((pid = fork()) == -1)
+		exit_fatal();
+	else if (pid == 0)
+	{
+		if (execve(av[0], av, envp) == -1)
+		ft_error("error: cannot execute ");
+		ft_error(av[0]);
+		ft_error("\n");
+		exit(1);
+	}
+	else
+		waitpid(pid, 0, 0);
+}
+void	fork_process(char **av, t_fd *fd, char **envp)
+{
+	pid_t pid;
+
+	if ((pid = fork()) == -1)
+		exit_fatal();
+	else if (pid == 0)
+	{
+		dup2(fd->write, 1);
+		close(fd->read);
+		start_process(av, envp);
+		close(fd->write);
+	}
+	else
+	{
+		dup2(fd->read, 0);
+		close(fd->write);
+		waitpid(pid, 0, 0);
+		close(fd->read);
+	}
+}
+
+void	exec_cmd(char **av, t_fd *fd, char **envp)
+{
+	if (fd->flag)
+		fork_process(av, fd, envp);
+	else
+		start_process(av, envp);
+}
+
+int mainv(int ac, char **av, char **envp)
+{
+	t_fd fd;
+	int i = 0;
+	int tmp_fd = dup(0);
+
+	(void)ac;
+	av++;
+	while (av[i++])
+	{
+		if (!strcmp("|", av[1]))
+		{
+			av[i] = 0;
+			if (i)
+			{
+				get_fd(&fd);
+				exec_cmd(av, &fd, envp);
+			}
+			av += i + 1;
+			i = 0;
+		}
+		else if (!strcmp(";", av[1]))
+		{
+			av[i] = 0;
+			if (i)
+				exec_cmd(av, &fd, envp);
+			av += i + 1;
+			i = 0;
+			dup2(tmp_fd, 0);
+		}
+		else if (!av[i + 1])
+		{
+			exec_cmd(av, &fd, envp);
+			av += i + 1;
+			i = 0;
+			dup2(tmp_fd, 0);
+		}
+		else
+			++i;
+	}
+	close(tmp_fd);
+	return (0);
+}
